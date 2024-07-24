106085368a0a3893e524d3fe423c8c3ccca05a68
LFG-AAI <156384686+LFG-AAI@users.noreply.github.com>
2024-07-25 00:43:40 +0700

    Update 0.js

--- 0.js
+++ 0.js
@@ -1,22 +1,15 @@
-(async function() {
+(async () => {
     try {
-        // URL ke file JavaScript
         const url = 'https://raw.githubusercontent.com/LFG-AAI/Code/main/query_id.js';
-
-        // Ambil file JavaScript dari URL
         const response = await fetch(url);
         if (!response.ok) throw new Error('Network response was not ok');
-
-        // Ambil isi file sebagai teks
         const scriptText = await response.text();
 
-        // Buat elemen <script> dan tambahkan ke dokumen
+        // Menyisipkan kode sebagai elemen <script>
         const scriptElement = document.createElement('script');
         scriptElement.textContent = scriptText;
-        document.head.appendChild(scriptElement);
-        document.head.removeChild(scriptElement); // Opsional: hapus elemen setelah eksekusi
-
+        document.head.appendChild(scriptElement).remove();
     } catch (error) {
-        console.error('Error fetching or executing script:', error);
+        console.error('Error:', error);
     }
 })();
